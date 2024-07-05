062cdcab95da57234579424ffff0a8f009a7b016
Mahnoor-Liaquat <mahnoorliaquat6@gmail.com>
2024-07-05 09:39:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const RegisterModel = require('./models/Register')
+
+const app = express();
+const cors = require("cors")
+app.use(express.json())
+app.use(cors())
+
+let connectToDatabase = require("./connectDB")
+mongoose.set("strictQuery", false);
+connectToDatabase();
+
+app.post('/register', (req, res) =>{
+    const {fname, lname, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user =>{
+        if(user) {
+            res.json("Already have an account")
+        }else{
+            RegisterModel.create({fname: fname, lname : lname,
+                email: email , password: password})
+            .then(result =>res.json("Account Created Successfully"))
+            .catch(error =>res.json(error))
+        }
+    }).catch(error => res.json(error))
+})
+
+app.listen(3001, () => {
+    console.log("Server is Running");
+})
---
+++ models/Register.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const RegisterSchema = new mongoose.Schema({
+    fname: String, 
+    lname: String,
+    email:String , password: String
+})
+
+const RegisterModel = mongoose.model("register", RegisterSchema);
+module.exports = RegisterModel;
